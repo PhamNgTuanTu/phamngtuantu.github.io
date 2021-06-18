eb7d05edc0ac81975b617c22ca7079ebda7b2bc3
bug_26.1 <trieudo.thuctapsinh@gmail.com>
2021-06-18 14:45:18 +0700

    commit_18_06

--- js/main.js
+++ js/main.js
@@ -85,4 +85,51 @@
     });
     // --toggle-tab-- 
 
+    /* show and hide the password */
+    const reg_eyes = document.querySelectorAll(".eye");
+
+    reg_eyes.forEach((eye) => {
+        eye.addEventListener("click", () => {
+            const pass_field = eye.previousElementSibling.previousElementSibling;
+            if (pass_field.type === "password") {
+                pass_field.setAttribute("type", "text");
+                eye.classList.replace("fa-eye-slash", "fa-eye");
+            } else {
+                pass_field.setAttribute("type", "password");
+                eye.classList.replace("fa-eye", "fa-eye-slash");
+            }
+        });
+    });
+    /* --show and hide the password-- */
+
+    // move login 
+    const loginBtn = document.getElementById('login');
+        const signupBtn = document.getElementById('signup');
+
+        loginBtn.addEventListener('click', (e) => {
+            let parent = e.target.parentNode.parentNode;
+            Array.from(e.target.parentNode.parentNode.classList).find((element) => {
+                if(element !== "slide-up") {
+                    parent.classList.add('slide-up')
+                }else{
+                    signupBtn.parentNode.classList.add('slide-up')
+                    parent.classList.remove('slide-up')
+                }
+            });
+        });
+
+        signupBtn.addEventListener('click', (e) => {
+            let parent = e.target.parentNode;
+            Array.from(e.target.parentNode.classList).find((element) => {
+                if(element !== "slide-up") {
+                    parent.classList.add('slide-up')
+                }else{
+                    loginBtn.parentNode.parentNode.classList.add('slide-up')
+                    parent.classList.remove('slide-up')
+                }
+            });
+        });
+    // --move login--
+
+
 })();
