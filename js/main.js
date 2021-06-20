7a4f6f3fe8fa5f8a027cff20e49751e96ce178d8
bug_26.1 <trieudo.thuctapsinh@gmail.com>
2021-06-20 16:20:53 +0700

    commit_19_06

--- js/main.js
+++ js/main.js
@@ -1,135 +1,140 @@
 (() => {
-    // wow
-    new WOW().init();
-    // --wow--
+  // wow
+  new WOW().init();
+  // --wow--
 
-    // progress-bar
-    var progress = document.getElementById("progressbar");
-    var totalHeight = document.body.scrollHeight - window.innerHeight;
-    window.onscroll = function() {
-        var progressHeight = (window.pageYOffset / totalHeight) * 100;
-        progress.style.height = progressHeight + "%";
-    };
-    // --progress-bar--
+  // progress-bar
+  var progress = document.getElementById("progressbar");
+  var totalHeight = document.body.scrollHeight - window.innerHeight;
+  window.onscroll = function () {
+    var progressHeight = (window.pageYOffset / totalHeight) * 100;
+    progress.style.height = progressHeight + "%";
+  };
+  // --progress-bar--
 
-    // giỏ hàng và duy chuyển lên đầu trang
+  // giỏ hàng và duy chuyển lên đầu trang
 
-    // giỏ hàng
-    window.addEventListener("scroll", function() {
-        var shopping_cart = document.querySelector(".shopping-cart");
-        shopping_cart.classList.toggle(
-            "active-scroll-shopping",
-            window.scrollY > 1
-        );
+  // giỏ hàng
+  window.addEventListener("scroll", function () {
+    var shopping_cart = document.querySelector(".shopping-cart");
+    shopping_cart.classList.toggle(
+      "active-scroll-shopping",
+      window.scrollY > 1
+    );
+  });
+  // --giỏ hàng--
+
+  // scroll to top
+  var scroll_top = document.querySelector(".scroll-top");
+  window.addEventListener("scroll", function () {
+    scroll_top.classList.toggle("active-scroll", window.scrollY > 500);
+  });
+  scroll_top.addEventListener("click", function () {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
     });
-    // --giỏ hàng--
+  });
+  // --scroll to top--
 
-    // scroll to top
-    var scroll_top = document.querySelector(".scroll-top");
-    window.addEventListener("scroll", function() {
-        scroll_top.classList.toggle("active-scroll", window.scrollY > 500);
+  // --giỏ hàng và duy chuyển lên đầu trang--
+
+  // loading
+  $(window).on("load", function () {
+    $(".loader").fadeOut();
+    $("#preloder").delay(200).fadeOut("slow");
+  });
+  // //loading
+
+  // tắt animation trên tablet & mobile
+  if ($(window).width() <= 991) {
+    $(".wow").removeClass("wow");
+    $(".animated").removeClass("animated");
+  }
+  // --tắt animation trên tablet & mobile--
+
+  // click to content
+  $("#view-add").click(function () {
+    var menu_tab = document.querySelector(
+      "#container-tile .list-tittle a:nth-child(1)"
+    );
+    var width = document.querySelector(
+      "#container-tile .list-tittle a:nth-child(1)"
+    ).offsetWidth;
+    $("#container-tile .slider").css({
+      left: 0,
+      width: width,
     });
-    scroll_top.addEventListener("click", function() {
-        window.scrollTo({
-            top: 0,
-            behavior: "smooth",
-        });
+    $("#container-tile .list-tittle a").removeClass("active");
+    $("#add-active").addClass("active");
+    $(".tab-slider--body").hide();
+    $(".tab-slider--body:first").show();
+
+    $("html, body").animate(
+      {
+        scrollTop: $("#introduce-book").offset().top - 195,
+      },
+      700
+    );
+  });
+  // --click to content--
+
+  // toggle-tab
+  $("document").ready(function () {
+    $(".tab-slider--body").hide();
+    $(".tab-slider--body:first").show();
+  });
+
+  $(".list-tittle li").click(function () {
+    $(".tab-slider--body").hide();
+    var activeTab = $(this).attr("rel");
+    $("#" + activeTab).fadeIn();
+  });
+  // --toggle-tab--
+
+  /* show and hide the password */
+  const reg_eyes = document.querySelectorAll(".eye");
+
+  reg_eyes.forEach((eye) => {
+    eye.addEventListener("click", () => {
+      const pass_field = eye.previousElementSibling.previousElementSibling;
+      if (pass_field.type === "password") {
+        pass_field.setAttribute("type", "text");
+        eye.classList.replace("fa-eye-slash", "fa-eye");
+      } else {
+        pass_field.setAttribute("type", "password");
+        eye.classList.replace("fa-eye", "fa-eye-slash");
+      }
     });
-    // --scroll to top--
+  });
+  /* --show and hide the password-- */
 
-    // --giỏ hàng và duy chuyển lên đầu trang--
+  // move login
+  const loginBtn = document.getElementById("login");
+  const signupBtn = document.getElementById("signup");
 
-    // loading
-    $(window).on("load", function() {
-        $(".loader").fadeOut();
-        $("#preloder").delay(200).fadeOut("slow");
+  loginBtn.addEventListener("click", (e) => {
+    let parent = e.target.parentNode.parentNode;
+    Array.from(e.target.parentNode.parentNode.classList).find((element) => {
+      if (element !== "slide-up") {
+        parent.classList.add("slide-up");
+      } else {
+        signupBtn.parentNode.classList.add("slide-up");
+        parent.classList.remove("slide-up");
+      }
     });
-    // //loading
+  });
 
-    // tắt animation trên tablet & mobile
-    if ($(window).width() <= 991) {
-        $(".wow").removeClass("wow");
-        $(".animated").removeClass("animated");
-    }
-    // --tắt animation trên tablet & mobile--
-
-    // click to content 
-    $("#view-add").click(function() {
-        var menu_tab = document.querySelector('#container-tile .list-tittle a:nth-child(1)');
-        var width = document.querySelector('#container-tile .list-tittle a:nth-child(1)').offsetWidth;
-        $("#container-tile .slider").css({
-            "left": 0,
-            "width": width
-        });
-        $("#container-tile .list-tittle a").removeClass('active');
-        $("#add-active").addClass('active');
-        $(".tab-slider--body").hide();
-        $(".tab-slider--body:first").show();
-
-        $('html, body').animate({
-            scrollTop: $("#introduce-book").offset().top - 195
-        }, 700);
+  signupBtn.addEventListener("click", (e) => {
+    let parent = e.target.parentNode;
+    Array.from(e.target.parentNode.classList).find((element) => {
+      if (element !== "slide-up") {
+        parent.classList.add("slide-up");
+      } else {
+        loginBtn.parentNode.parentNode.classList.add("slide-up");
+        parent.classList.remove("slide-up");
+      }
     });
-    // --click to content--
-
-    // toggle-tab 
-    $("document").ready(function() {
-        $(".tab-slider--body").hide();
-        $(".tab-slider--body:first").show();
-    });
-
-    $(".list-tittle li").click(function() {
-        $(".tab-slider--body").hide();
-        var activeTab = $(this).attr("rel");
-        $("#" + activeTab).fadeIn();
-    });
-    // --toggle-tab-- 
-
-    /* show and hide the password */
-    const reg_eyes = document.querySelectorAll(".eye");
-
-    reg_eyes.forEach((eye) => {
-        eye.addEventListener("click", () => {
-            const pass_field = eye.previousElementSibling.previousElementSibling;
-            if (pass_field.type === "password") {
-                pass_field.setAttribute("type", "text");
-                eye.classList.replace("fa-eye-slash", "fa-eye");
-            } else {
-                pass_field.setAttribute("type", "password");
-                eye.classList.replace("fa-eye", "fa-eye-slash");
-            }
-        });
-    });
-    /* --show and hide the password-- */
-
-    // move login 
-    const loginBtn = document.getElementById('login');
-        const signupBtn = document.getElementById('signup');
-
-        loginBtn.addEventListener('click', (e) => {
-            let parent = e.target.parentNode.parentNode;
-            Array.from(e.target.parentNode.parentNode.classList).find((element) => {
-                if(element !== "slide-up") {
-                    parent.classList.add('slide-up')
-                }else{
-                    signupBtn.parentNode.classList.add('slide-up')
-                    parent.classList.remove('slide-up')
-                }
-            });
-        });
-
-        signupBtn.addEventListener('click', (e) => {
-            let parent = e.target.parentNode;
-            Array.from(e.target.parentNode.classList).find((element) => {
-                if(element !== "slide-up") {
-                    parent.classList.add('slide-up')
-                }else{
-                    loginBtn.parentNode.parentNode.classList.add('slide-up')
-                    parent.classList.remove('slide-up')
-                }
-            });
-        });
-    // --move login--
-
-
+  });
+  // --move login--
 })();
--- js/nav-menu.js
+++ js/nav-menu.js
@@ -1,10 +1,9 @@
-(() =>{
- 
+(() => {
   const openNavMenu = document.querySelector(".open-nav-menu"),
-  closeNavMenu = document.querySelector(".close-nav-menu"),
-  navMenu = document.querySelector(".nav-menu"),
-  menuOverlay = document.querySelector(".menu-overlay"),
-  mediaSize = 991;
+    closeNavMenu = document.querySelector(".close-nav-menu"),
+    navMenu = document.querySelector(".nav-menu"),
+    menuOverlay = document.querySelector(".menu-overlay"),
+    mediaSize = 991;
 
   openNavMenu.addEventListener("click", toggleNav);
   closeNavMenu.addEventListener("click", toggleNav);
@@ -12,55 +11,56 @@
   menuOverlay.addEventListener("click", toggleNav);
 
   function toggleNav() {
-  	navMenu.classList.toggle("open");
-  	menuOverlay.classList.toggle("active");
-  	document.body.classList.toggle("hidden-scrolling");
+    navMenu.classList.toggle("open");
+    menuOverlay.classList.toggle("active");
+    document.body.classList.toggle("hidden-scrolling");
   }
 
-  navMenu.addEventListener("click", (event) =>{
-      if(event.target.hasAttribute("data-toggle") && 
-      	window.innerWidth <= mediaSize){
-      	// ngăn chặn nhấp chuột
-      	event.preventDefault();
-      	const menuItemHasChildren = event.target.parentElement;
-        // nếu menuItemHasChildren được mở ra, thì đóng
-        if(menuItemHasChildren.classList.contains("active")){
-        	collapseSubMenu();
+  navMenu.addEventListener("click", (event) => {
+    if (
+      event.target.hasAttribute("data-toggle") &&
+      window.innerWidth <= mediaSize
+    ) {
+      // ngăn chặn nhấp chuột
+      event.preventDefault();
+      const menuItemHasChildren = event.target.parentElement;
+      // nếu menuItemHasChildren được mở ra, thì đóng
+      if (menuItemHasChildren.classList.contains("active")) {
+        collapseSubMenu();
+      } else {
+        // thu gọn menu mở rộng hiện có
+        if (navMenu.querySelector(".menu-item-has-children.active")) {
+          collapseSubMenu();
         }
-        else{
-          // thu gọn menu mở rộng hiện có
-          if(navMenu.querySelector(".menu-item-has-children.active")){
-        	collapseSubMenu();
-          }
-          // mở rộng menu mới
-          menuItemHasChildren.classList.add("active");
-          const subMenu = menuItemHasChildren.querySelector(".sub-menu");
-          subMenu.style.maxHeight = subMenu.scrollHeight + "px";
-        }
+        // mở rộng menu mới
+        menuItemHasChildren.classList.add("active");
+        const subMenu = menuItemHasChildren.querySelector(".sub-menu");
+        subMenu.style.maxHeight = subMenu.scrollHeight + "px";
       }
+    }
   });
-  function collapseSubMenu(){
-  	navMenu.querySelector(".menu-item-has-children.active .sub-menu")
-  	.removeAttribute("style");
-  	navMenu.querySelector(".menu-item-has-children.active")
-  	.classList.remove("active");
+  function collapseSubMenu() {
+    navMenu
+      .querySelector(".menu-item-has-children.active .sub-menu")
+      .removeAttribute("style");
+    navMenu
+      .querySelector(".menu-item-has-children.active")
+      .classList.remove("active");
   }
-  function resizeFix(){
-  	 // đóng menu con đang mở
-  	 if(navMenu.classList.contains("open")){
-  	 	toggleNav();
-  	 }
-  	 // nếu menuItemHasChildren được mở ra, thì đóng
-  	 if(navMenu.querySelector(".menu-item-has-children.active")){
-        	collapseSubMenu();
-     }
+  function resizeFix() {
+    // đóng menu con đang mở
+    if (navMenu.classList.contains("open")) {
+      toggleNav();
+    }
+    // nếu menuItemHasChildren được mở ra, thì đóng
+    if (navMenu.querySelector(".menu-item-has-children.active")) {
+      collapseSubMenu();
+    }
   }
 
-  window.addEventListener("resize", function(){
-     if(this.innerWidth > mediaSize){
-     	resizeFix();
-     }
+  window.addEventListener("resize", function () {
+    if (this.innerWidth > mediaSize) {
+      resizeFix();
+    }
   });
-
 })();
-
